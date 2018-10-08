29a250634b3cf3707fb541ba149c08d57b3c463a
PickAUsername15 <rotdob16@new.edvhtl.at>
2018-10-08 10:24:15 +0200

    Bl

--- Jg3_009_Anlagenverzeichniss/src/AnlagenverzeichnissBl.java
+++ Jg3_009_Anlagenverzeichniss/src/AnlagenverzeichnissBl.java
@@ -1,14 +1,48 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author User
  */
-public class AnlagenverzeichnissBl
-{
+public class AnlagenverzeichnissBl extends AbstractTableModel {
+
+    private ArrayList<Anlage> anlagen = new ArrayList<>();
+    private String COLNAMES[] = {"Bezeichnung", "AK", "Inbetriebnahme", "ND", "bish. ND", "AfA bisher", "Wert vor AfA", "AfA d.J.", "BW 31.12"};
+
+    public void calc(int year) {
+
+    }
+
+    public void load (File f){
+        
+    }
+    @Override
+    public int getRowCount() {
+        return anlagen.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return COLNAMES.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return COLNAMES[column]; //To change body of generated methods, choose Tools | Templates.
+    }
+
     
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return anlagen.get(columnIndex);
+    }
+
 }
